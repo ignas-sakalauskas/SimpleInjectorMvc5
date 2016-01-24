b1a83161cd2d06ed4990032161630600e1350092
Ignas Sakalauskas <ignas.sakal@outlook.com>
2016-01-24 13:49:54 +0000

    Add initial source code.

---
+++ WebApplication1/WebApplication1.Contracts/ITestService.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Contracts
+{
+    public interface ITestService
+    {
+        int GetTest();
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using WebApplication1.Contracts;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITestService _testService;
+
+        public HomeController(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Test = _testService.GetTest();
+            return View();
+        }
+    }
+}
