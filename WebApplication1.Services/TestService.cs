d93f707e8d0d6db8ed012bb675c605affc6a7236
Ignas <ignas.sakal@outlook.com>
2016-04-17 19:34:42 +0100

    Update folder structure.

---
+++ WebApplication1.Services/TestService.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Contracts;
+
+namespace WebApplication1.Services
+{
+    public class TestService : ITestService
+    {
+        public int GetTest()
+        {
+            return 111;
+        }
+    }
+}
--- WebApplication1/WebApplication1.Contracts/ITestService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebApplication1.Contracts
-{
-    public interface ITestService
-    {
-        int GetTest();
-    }
-}
